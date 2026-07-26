61653a0831c997916ee5d940807eb81e35ed8bde
Alex Yagodkin <adamsHillzzz@gmail.com>
2026-07-26 12:28:04 +0300

    feat: add for macos lock handler

--- src/platform/single_instance_posix.c
+++ src/platform/single_instance_posix.c
@@ -5,10 +5,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 #include <errno.h>
 #include <sys/stat.h>
-#include <SDL3/SDL.h>
+#include <stdbool.h>
 
 static int fileHandle = -1;
 
@@ -22,6 +21,11 @@ SingleInstanceResult singleInstanceAcquire(void)
     }
 
     char* runtimeDir = malloc(runtimeDirSize);
+    if (runtimeDir == NULL)
+    {
+        return SINGLE_INSTANCE_ERROR;
+    }
+
     size_t runtimeWritten = confstr(_CS_DARWIN_USER_TEMP_DIR, runtimeDir, runtimeDirSize);
     if (runtimeWritten == 0 || runtimeWritten > runtimeDirSize || runtimeDir[0] == '\0')
     {
@@ -36,6 +40,12 @@ SingleInstanceResult singleInstanceAcquire(void)
     size_t lockPathSize = dirLength + (hasSlash ? 0 : 1) + fileLength + strlen(".lock") + 1;
 
     char* lockPath = malloc(lockPathSize);
+    if (lockPath == NULL)
+    {
+        free(runtimeDir);
+        return SINGLE_INSTANCE_ERROR;
+    }
+
     int lockWritten =
         snprintf(lockPath, lockPathSize, hasSlash ? "%s%s.lock" : "%s/%s.lock", runtimeDir, GAME_INSTANCE_ID);
     if (lockWritten < 0 || (size_t)lockWritten >= lockPathSize)
@@ -66,10 +76,42 @@ SingleInstanceResult singleInstanceAcquire(void)
     }
 #endif
 
-    // fileHandle = open();
+    fileHandle = open(lockPath, O_RDWR | O_CREAT | O_CLOEXEC, 0600);
+    free(lockPath);
+
+    if (fileHandle == -1)
+    {
+        return SINGLE_INSTANCE_ERROR;
+    }
+
+    struct flock lock = {0};
+    lock.l_type = F_WRLCK;
+    lock.l_whence = SEEK_SET;
+    lock.l_start = 0;
+    lock.l_len = 0;
+
+    if (fcntl(fileHandle, F_SETLK, &lock) == -1)
+    {
+        if (errno == EACCES || errno == EAGAIN)
+        {
+            singleInstanceRelease();
+
+            return SINGLE_INSTANCE_ALREADY_RUNNING;
+        }
+
+        singleInstanceRelease();
+
+        return SINGLE_INSTANCE_ERROR;
+    }
+
     return SINGLE_INSTANCE_ACQUIRED;
 }
 
 void singleInstanceRelease(void)
 {
+    if (fileHandle != -1)
+    {
+        close(fileHandle);
+        fileHandle = -1;
+    }
 }
